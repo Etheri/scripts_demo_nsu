9f350e7533e479ef086dcb61c12ece1e846b4a80
Anastasia <bednyakova.anastasia@gmail.com>
2016-10-13 18:09:13 +0700

    Init

---
+++ repeat_str.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+file_name=$1
+string=$2
+count=$3
+
+if [ -f $file_name ]; then
+    echo "File exists"
+    exit 0
+fi
+
+for n in $(seq $3);
+do
+    echo "$string" >> $file_name
+    echo "$n"
+done
+
